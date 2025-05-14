96050f305c4541125a5ee4b2d166a37de167c9f0
edoh-10 <jeremieedoh9@gmail.com>
2025-05-14 17:35:05 +0200

    Mise à jour des liens api dans le front end

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./Context/AuthContext";
+import { PublicsNotesProvider } from "./Context/PublicsNotesContext";
+import { UserProfilProvider } from "./Context/UserProfileContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AuthProvider>
+  <UserProfilProvider>
+    <PublicsNotesProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    </PublicsNotesProvider>
+  </UserProfilProvider>
+  </AuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
